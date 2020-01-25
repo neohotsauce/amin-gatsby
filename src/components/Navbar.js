81fb5987faaaa5394c28b968b0cabf4d1ab0bbde
neo_hotsauce <alihanyn@hotmail.com>
2020-01-25 22:58:03 +0500

    fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState, useRef } from "react";
 import { Link } from "gatsby";
 import logo from "../img/logo.png";
 
 const Navbar = ({ location, pageContext }) => {
+  const navCollapse = useRef(null);
+
   const [pagePath, setPath] = useState("");
+  const [isExpanded, expand] = useState(false);
+
+  const toggleShow = () => {
+    if (isExpanded) {
+      return expand(false);
+    }
+    return expand(true);
+  };
 
   const page = pageContext.templateKey || { templateKey: null };
 
@@ -11,10 +21,33 @@ const Navbar = ({ location, pageContext }) => {
     setPath(location ? location.pathname.substr(1) : "404");
   }, [location]);
 
+  useEffect(() => {
+    if (isExpanded) {
+      setTimeout(() => {
+        return navCollapse.current.classList.remove("collapsing");
+      }, 500);
+      setTimeout(() => {
+        return (navCollapse.current.style.height = "100vh");
+      }, 1);
+      navCollapse.current.classList.add("show");
+      return navCollapse.current.classList.add("collapsing");
+    }
+    // collapse
+    setTimeout(() => {
+      navCollapse.current.classList.remove("show");
+      return navCollapse.current.classList.remove("collapsing");
+    }, 500);
+    setTimeout(() => {
+      return (navCollapse.current.style.height = "0");
+    }, 1);
+
+    return navCollapse.current.classList.add("collapsing");
+  }, [isExpanded]);
+
   return (
     <Fragment>
       <nav
-        className="navbar navbar-expand-md navbar-dark fixed-top"
+        className="navbar navbar-expand-lg navbar-dark fixed-top"
         id="banner"
       >
         <Link className="navbar-brand" to="/">
@@ -22,15 +55,23 @@ const Navbar = ({ location, pageContext }) => {
         </Link>
 
         <button
+          onClick={toggleShow}
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#collapsibleNavbar"
+          // data-toggle="collapse"
+          // data-target="#collapsibleNavbar"
+          // aria-controls="collapsibleNavbar"
+          // aria-expanded="false"
+          // aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="collapsibleNavbar">
+        <div
+          ref={navCollapse}
+          className="collapse navbar-collapse"
+          id="collapsibleNavbar"
+        >
           <ul className="navbar-nav ml-auto">
             <li className={pagePath === "" ? "nav-item active" : "nav-item"}>
               <Link className="nav-link" to="/">
